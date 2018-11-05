08558bf409d109be47e321669a8d1205788dcb9d
rcmayhew <rcmayhewii@gmail.com>
2018-11-05 14:14:05 -0800

    updated comments module

--- src/reviews.py
+++ src/reviews.py
@@ -15,19 +15,42 @@ def check_bus_id(review, biz_id="JNXUYY8wbaaDmk3BPzlWw"):
 
 
 def tuple_data(review):
+    comment_cap = comment_end(review)
     stars = review[120]
-    text = review[151:600]
+    text = review[150:comment_cap]
     return text, stars
+
+
+def check_end(review, location):
+    seg = review[location + 1: location + 4]
+    if seg == '","':
+        return False
+    return True
+
+
+def comment_end(review):
+    location = 150
+    end_not_reached = check_end(review, location)
+    while end_not_reached:
+        location = location + 1
+        end_not_reached = check_end(review, location)
+    return location
 
 
 def get_reviews(biz_id):
     data = []
     loop = 0
+    limiter = 0
+    threshold = 10000
     for line in open(json_review, encoding="utf8"):
         if loop % 100000 == 0:
             print("loop: %s" % loop)
         if check_bus_id(line, biz_id):
-            data.append(tuple_data(line))
+            couple = tuple_data(line)
+            data.append(couple)
+            limiter = limiter + 1
+        if limiter > threshold:
+            break
         loop = loop + 1
 
     print(len(data))
@@ -40,4 +63,4 @@ def add_data(data, name_of_var):
 
 
 review_list = get_reviews(tested_id)
-add_data(review_list, "selected_reviews")
+# add_data(review_list, "selected_reviews")
