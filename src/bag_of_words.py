23bcfc24196208a6713de3221af53e84609567b4
rcmayhew <rcmayhewii@gmail.com>
2018-11-06 17:05:33 -0800

    added a word parser

--- src/bag_of_words.py
+++ src/bag_of_words.py
@@ -50,19 +50,61 @@ def graphing():
 
 
 def find_word(review, start):
-    
+    place = start
+    while place < len(review):
+        space, length = find_space(review, place)
+        if space and place == start:
+            start = start + length
+        elif space:
+            break
+        place = place + 1
+    word = review[start:place]
+    print(word)
+    return word, place
 
 
 def find_space(review, location):
-    if review[location] == ' ':
-        return True
-    return False
+    char = review[location]
+    long_char = review[location:location + 2]
+    med_char = review[location:location + 1]
+
+    single_char_punctuation = [' ', '.', ',', ';', '!', '?', '(', ')', '-']
+    med_char_punctuation = ['--']
+    long_char_punctuation = ["\\n", '\\"']
+
+    if char in single_char_punctuation:
+        return True, 1
+    if med_char is med_char_punctuation:
+        return True, 2
+    if long_char in long_char_punctuation:
+        return True, 3
+
+    return False, 0
+
+# need to convert words to all lower case to see the ammount of words
+# then order the words
 
 
 def word_count():
+    next_start = 0
+    words = {}
+    loop = 0
     for x in data:
-        y = x
-    print(y[0])
+        if loop % 1000:
+            print(loop)
+        loop = loop + 1
+        if loop < 8000:
+            review = x[0]
+            while next_start < len(review):
+                word, next_start = find_word(review, next_start)
+                if word in words:
+                    words[word] = words[word] + 1
+                else:
+                    words[word] = 1
+            next_start = 0
+        else:
+            break
+    print(words)
 
 
 word_count()
