16bf552cffaa2d91f0c08d70151220998db79ac3
rcmayhew <rcmayhewii@gmail.com>
2018-11-18 14:10:43 -0800

    added voting list

--- src/methods/KNN.py
+++ src/methods/KNN.py
@@ -18,10 +18,12 @@ class KNN:
         self.removed_features = []
         self.class_number = 0
         self.k = 0
+        self.class_data = []
         self.data = []
 
-    def __load_data__(self, np_data_array:np.array):
+    def __load_data__(self, np_data_array: np.array):
         self.data = np_data_array
+        self.class_data = np.delete(self.data, -1, axis=1)
 
     def train(self, data):
         """
@@ -52,10 +54,10 @@ class KNN:
         """
 
     @staticmethod
-    def __distance__(a:np.array, b:np.array):
+    def __distance__(a: np.array, b: np.array):
         return np.linalg.norm(a-b)
 
-    def __test_voting_point__(self, voting_points:list, trial_point:tuple):
+    def __test_voting_point__(self, voting_points: list, trial_point: tuple):
         """
         takes a testing point and checks if the new point is a
         contender to classify the instance being classified
@@ -73,14 +75,30 @@ class KNN:
         else:
             return voting_points, False
 
-    def __clean_voting_list__(self, voting_list):
+    @staticmethod
+    def __clean_voting_list__(voting_list):
+        """
+        can assume list size is k + 1
+        :param voting_list:
+        :return:
+        """
         voting_list = sorted(voting_list, key=lambda x: x[0])
         voting_list.pop()
         return voting_list
 
+    def __make_voting_list(self, test_vector):
+        voting_list = []
+        for index, row in enumerate(self.data):
+            space = self.__distance__(row, test_vector)
+            data_group = space, self.class_data[index]
+            self.__test_voting_point__(voting_list, data_group)
+        return voting_list
+
+    def __tally_votes(self):
+        pass
 
     def __single_instance_classify(self, test_vector):
-
+        voting_list = self.__make_voting_list(test_vector)
 
     def classify(self, testing_data):
         pass
